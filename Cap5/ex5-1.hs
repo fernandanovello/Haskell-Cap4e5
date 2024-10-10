9806039b7d4756fe1f20aa518db12cf15eda5c3c
Fernanda Novello <110908782+fernandanovello@users.noreply.github.com>
2024-10-10 03:43:49 -0300

    Update ex5-1.hs

--- Cap5/ex5-1.hs
+++ Cap5/ex5-1.hs
@@ -1,2 +1,13 @@
-primeiro :: (a, b) -> a
-primeiro (x, _) = x
+data TipoProduto = Escritorio | Informatica | Livro | Filme | Total deriving (Show, Eq)
+
+data Produto = Produto { valor :: Double, tp :: TipoProduto } 
+             | Nada 
+             deriving (Show, Eq)
+
+instance Semigroup Produto where
+    (Produto v1 _) <> (Produto v2 _) = Produto (v1 + v2) Total
+    Nada <> p = p
+    p <> Nada = p
+
+instance Monoid Produto where
+    mempty = Nada
